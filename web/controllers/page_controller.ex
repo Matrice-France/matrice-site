ffc08583f93d544d769bd127b91862ccd3b73484
Andre Aubin <andre@andral.kiwi>
2017-04-01 17:21:41 +0200

    footer

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,39 +5,39 @@ defmodule Matrice.PageController do
     render conn, "index.html"
   end
 
-  def pedagogie(conn, _params) do
-    render conn, "pedagogie.html"
-  end
-
   def philosophie(conn, _params) do
     render conn, "philosophie.html"
   end
-
-  def processus(conn, _params) do
-    render conn, "processus.html"
-  end
-
-  def en_action(conn, _params) do
-    en_action_autisme(conn, _params)
-  end
-
-  def en_action_autisme(conn, _params) do
-    render conn, "en_action_autisme.html"
-  end
-
-  def en_action_art(conn, _params) do
-    render conn, "en_action_art.html"
-  end
-
-  def en_action_cpa(conn, _params) do
-    render conn, "en_action_cpa.html"
-  end
-
-  def en_action_si(conn, _params) do
-    render conn, "en_action_si.html"
-  end
-
-  def matrice_et_vous(conn, _params) do
-    render conn, "matrice_et_vous.html"
-  end
+  #
+  # def pedagogie(conn, _params) do
+  #   render conn, "pedagogie.html"
+  # end
+  #
+  # def processus(conn, _params) do
+  #   render conn, "processus.html"
+  # end
+  #
+  # def en_action(conn, _params) do
+  #   en_action_autisme(conn, _params)
+  # end
+  #
+  # def en_action_autisme(conn, _params) do
+  #   render conn, "en_action_autisme.html"
+  # end
+  #
+  # def en_action_art(conn, _params) do
+  #   render conn, "en_action_art.html"
+  # end
+  #
+  # def en_action_cpa(conn, _params) do
+  #   render conn, "en_action_cpa.html"
+  # end
+  #
+  # def en_action_si(conn, _params) do
+  #   render conn, "en_action_si.html"
+  # end
+  #
+  # def matrice_et_vous(conn, _params) do
+  #   render conn, "matrice_et_vous.html"
+  # end
 end
